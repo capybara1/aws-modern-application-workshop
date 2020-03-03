78337852840577bb4739f91b2c2cd82b41431080
capybara1 <39344016+capybara1@users.noreply.github.com>
2020-03-03 07:14:18 +0100

    Improved path handling

--- module-2/terraform/deploy/update_content.sh
+++ module-2/terraform/deploy/update_content.sh
@@ -1,7 +1,8 @@
 set -eu
 
-MODULE_1_DIR="../../../module-1"
-MODULE_2_DIR="../.."
-S3_BUCKET_NAME=$(terraform output -state="$MODULE_1_DIR/terraform/terraform.tfstate" static_content_bucket_name)
+REPO_DIR=$(git rev-parse --show-toplevel)
+MODULE_1_DIR="REPO_DIR/module-1"
+MODULE_2_DIR="REPO_DIR/module-2"
+S3_BUCKET_NAME=$(terraform output -state="$REPO_DIR/module-1/terraform/terraform.tfstate" static_content_bucket_name)
 API_ENDPOINT_URL=$(terraform output api_endpoint_url)
-sed "s/REPLACE_ME/http:\/\/$API_ENDPOINT_URL/g" "$MODULE_2_DIR/web/index.html" | aws s3 cp - "s3://$S3_BUCKET_NAME/index.html" --content-type text/html
+sed "s/REPLACE_ME/http:\/\/$API_ENDPOINT_URL/g" "$REPO_DIR/module-2/web/index.html" | aws s3 cp - "s3://$S3_BUCKET_NAME/index.html" --content-type text/html
